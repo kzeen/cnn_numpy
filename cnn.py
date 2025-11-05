7d14c31c29dae8788a42f8d269458e5ebdfe04e0
zeen <157370421+kzeen@users.noreply.github.com>
2025-11-05 16:32:07 +0100

    Add pooling layer to CNN

--- cnn.py
+++ cnn.py
@@ -1,11 +1,15 @@
 import numpy as np
 from tensorflow.keras.datasets import mnist # type: ignore
 from conv import Conv3x3
+from maxpool import MaxPool2
 
 # Using the MNIST dataset to test the CNN
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 conv = Conv3x3(8) # Conv layer with 8 filters
+pool = MaxPool2()
+
 output = conv.forward(train_images[0])
+output = pool.forward(output)
 
 print(output.shape)
